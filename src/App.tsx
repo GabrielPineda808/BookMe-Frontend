1dacd84ac087e70bb712910d1f95791336f99b07
Gabe <gabepineda6@gmail.com>
2025-10-01 19:16:23 -0700

    feat(app): wrapped routes with auth provider, browser router, and login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { AuthProvider } from "./auth/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello</h1>
-      </div>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>} />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
