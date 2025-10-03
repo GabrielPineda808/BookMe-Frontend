728cdc391578f7633570c3dd85bb3470af71ee0c
Gabe <gabepineda6@gmail.com>
2025-10-02 23:19:01 -0700

    feat(app): added privtes routes after login for homepage: '

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { AuthProvider } from "./auth/AuthContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Suspense } from "react";
 import Login from "./pages/Login";
+import PrivateRoute from "./route/PrivateRoute";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -9,6 +11,26 @@ function App() {
       <Suspense fallback={<div>Loading...</div>} />
       <Routes>
         <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AuthProvider>
   );
