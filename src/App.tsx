1f7313949cf92d0eb33fe9a2454cb4833b08cbe6
Gabe <gabepineda6@gmail.com>
2025-10-15 19:00:10 -0700

    feat(app): added routes under new header/nav component based on auth status

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { AuthProvider } from "./auth/AuthContext";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import { Suspense } from "react";
 import Login from "./pages/Login";
 import PrivateRoute from "./route/PrivateRoute";
@@ -8,38 +8,40 @@ import SignUp from "./pages/SignUp";
 import AccountVerification from "./pages/AccountVerification";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+import AuthLayout from "./components/AuthLayout";
+import MainLayout from "./components/MainLayout";
+import NotFoundRoute from "./route/NotFoundRoute";
+import DefaultRoute from "./route/DefaultRoute";
 
 function App() {
   return (
     <AuthProvider>
-      <Suspense fallback={<div>Loading...</div>} />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/verify" element={<AccountVerification />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<DefaultRoute />} />
 
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/verify" element={<AccountVerification />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/auth/*" element={<NotFoundRoute />} />
+          </Route>
 
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+          <Route element={<MainLayout />}>
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<NotFoundRoute />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </AuthProvider>
   );
 }
